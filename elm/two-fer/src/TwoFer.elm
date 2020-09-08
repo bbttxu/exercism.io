7f42eb9408e9c40a9ec3beffde9b7187dbfba74c
Adam Krawiec <adamkrawiec@gmail.com>
2020-09-08 01:26:39 +0000

    figure out let and case; key is indentation

--- elm/two-fer/src/TwoFer.elm
+++ elm/two-fer/src/TwoFer.elm
@@ -1,10 +1,13 @@
 module TwoFer exposing (twoFer)
 
-
 twoFer : Maybe String -> String
-twoFer name =
-    case name of
-        Nothing -> "One for you, one for me."
-        Just name1 -> "One for " ++ name1 ++ ", One for me."
-    
+twoFer input =
+    let 
+        output = 
+            case input of
+                Just name -> name
+                _ -> "you"
+
+    in "One for " ++ output ++ ", one for me."
+
     
